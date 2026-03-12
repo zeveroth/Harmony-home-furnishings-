072989c6c0ffe17a1c638612467f166704a485af
zeveroth <zeveroth1@gmail.com>
2026-03-12 00:14:20 -0500

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -12,7 +12,12 @@ function addToCart(product) {
   const cart = getCart();
   cart.push(product);
   saveCart(cart);
+
+  // update header counter
+  const el = document.getElementById("cart-count");
+  if (el) el.textContent = cart.length;
 }
+
 
 function removeFromCart(index) {
   const cart = getCart();
