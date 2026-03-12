7d3191397376de3d5212f0907dc30f9b33e6cbab
zeveroth <zeveroth1@gmail.com>
2026-03-12 00:15:22 -0500

    Update wishlist.js

--- js/wishlist.js
+++ js/wishlist.js
@@ -11,6 +11,17 @@ function saveWishlist(list) {
 function addToWishlist(product) {
   const list = getWishlist();
 
+  if (!list.find(item => item.id === product.id)) {
+    list.push(product);
+    saveWishlist(list);
+  }
+
+  // update header counter
+  const el = document.getElementById("wishlist-count");
+  if (el) el.textContent = list.length;
+}
+
+
   // prevent duplicates
   if (!list.find(item => item.id === product.id)) {
     list.push(product);
