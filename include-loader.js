2ab155815128bea261873e9d1e135c12a926c050
zeveroth <zeveroth1@gmail.com>
2026-03-19 17:26:31 -0500

    Delete include-loader.js

--- include-loader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("[data-include]").forEach(async el => {
-    const file = el.getAttribute("data-include");
-    const response = await fetch(file);
-    const html = await response.text();
-    el.outerHTML = html;
-  });
-});
